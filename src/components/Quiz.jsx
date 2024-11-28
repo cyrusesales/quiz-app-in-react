15e856750b80725cf92ab1d758939c4e68878849
Cyrus Sales <cyrus.sales@outlook.com>
2024-11-28 11:06:48 +0800

    shuffling answers & adding quiz logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import QUESTIONS from '../questions.js'
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
     // register the answer selected by users, want to store picked by user
@@ -8,6 +9,10 @@ export default function Quiz() {
 
     //if useState is empty array, activeQuestionIndex is zero
     const activeQuestionIndex = userAnswers.length;
+    
+    //make sure we cant exceed number of questions we have
+    // true or false
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     //add func trigger when button is press
     function handleSelectAnswer(selectedAnswer) {
@@ -20,6 +25,28 @@ export default function Quiz() {
         });
     }
 
+    //display diff content if quiz is over
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+
+    //----------only execute if we still have questions to display
+     //spread questions into this array, created new array so dont edit the original answer array
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    
+     //use shuffle and call built in sort, will no return new array, instead added array, creating a new copy by creating sort
+     //then take a fucntion , always receive two elements from array, if negative those element will swap and if positive, they stay in the order
+     // pair by pair to derive a new order
+     // want to shuffle the order here,
+     //Math random give us a value between 0 and 1 and with 0.5, will end up negative value in 50 or 100 cases or with the negative value
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
+    
     return (
         <div id="quiz">
             <div id="question">
@@ -27,7 +54,7 @@ export default function Quiz() {
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {/*output the possible answer, map list of answer to list of JSX components*/}
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             {/*
                             //output button should be selectable
