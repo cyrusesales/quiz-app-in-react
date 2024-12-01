7c1fd83df2ff1b4a57ae9e1fba354e183ea31be4
Cyrus Sales <cyrus.sales@outlook.com>
2024-12-01 21:29:32 +0800

    highlighting selected answer & managing more state

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,11 +5,15 @@ import QuestionTimer from './QuestionTimer.jsx';
 import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
+    const [answerState, setAnswerState] = useState('');
+
     // register the answer selected by users, want to store picked by user
     const [userAnswers, setUserAnswers] = useState([]);
 
     //if useState is empty array, activeQuestionIndex is zero
-    const activeQuestionIndex = userAnswers.length;
+    //make sure active question index if current answer state is empty string
+    //otherwise should be equal to useranswer length minus 1 to stick with old question
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
     
     //make sure we cant exceed number of questions we have
     // true or false
@@ -18,6 +22,10 @@ export default function Quiz() {
     //add func trigger when button is press
     // dont have dependecy, not user state or props
     const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+        //instead of storing answer right away, wants to change the color first the selected answer
+        // change answerState to 'answered' once user did select an answer
+        setAnswerState('answered');
+
         //store the selected answer in user array
         // update the user answer state
         // update the func form, bec wanna update the state based on prev state
@@ -25,7 +33,24 @@ export default function Quiz() {
             //return spread existing user answer but append prev answer
             return [...prevUserAnswer, selectedAnswer];
         });
-    }, []);
+
+        //set timeout to check if selected answer is correct or wrong
+        setTimeout(() => {
+            //compare to the correct answer
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+                setAnswerState('correct');
+            } else {
+                setAnswerState('wrong');
+            }
+
+            //only start after this timer stopped,setAnswerState is empty string, make sure answer is reset then move on next question
+            setTimeout(() => {
+                setAnswerState('');
+            }, 2000);
+
+        }, 1000);
+        //activeQuestionIndex should recreated whenever it changes so it add as dependency
+    }, [activeQuestionIndex]);
 
     //happen if timer expired, useCallback need dependecy that list depedency might use
     //handleSelectAnswer depends on props and state
@@ -69,16 +94,37 @@ export default function Quiz() {
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {/*output the possible answer, map list of answer to list of JSX components*/}
-                    {shuffledAnswers.map((answer) => (
-                        <li key={answer} className="answer">
-                            {/*
-                            //output button should be selectable
-                            //wrap anonymous function so we have more control over handleSelectAnswer will envoke, pass the answer here, not executed immediately
-                            //custom func executed
-                            */}
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    ))}
+                    {shuffledAnswers.map((answer) => {
+                            //look userAnswers.length minus 1
+                            const isSelected = userAnswers[userAnswers.length - 1] === answer;
+                            let cssClass = '';
+
+                            //check if answerState is equal to answered, also taking look userAnswer
+                            if (answerState === 'answered' && isSelected) {
+                                cssClass = 'selected';
+                            }
+
+                            if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
+                                cssClass = answerState;
+                            }
+
+                            return (
+                                <li key={answer} className="answer">
+                                {/*
+                                //output button should be selectable
+                                //wrap anonymous function so we have more control over handleSelectAnswer will envoke, pass the answer here, not executed immediately
+                                //custom func executed
+                                */}
+                                    <button 
+                                        onClick={() => handleSelectAnswer(answer)} 
+                                        className={cssClass}
+                                    >
+                                        {answer}
+                                    </button>
+                                </li>
+                            );
+                        } 
+                    )}
                 </ul>
             </div>
         </div>
